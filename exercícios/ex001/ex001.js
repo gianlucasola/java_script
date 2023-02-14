d5b412a28790e4492e4f6a32160d27d618791402
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-02-14 11:23:45 -0300

    Criação da Pasta ex001 para html e js

---
+++ exercícios/ex001/ex001.js
@@ -0,0 +1,7 @@
+window.alert('Bom dia!') /*  abrirá uma JANELA ALERTA com a frase */
+ 
+
+window.prompt('Qual é a sua idade?') /*  abrirá uma JANELA PROMPT com a frase */
+
+window.confirm('Confirme sua idade clicando em OK.') /*  abrirá uma JANELA DE CONFIRMAÇÃO com a frase */
+
