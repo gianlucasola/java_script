d22720fb12f16b99903fe30ead74895c458fc93e
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-02-14 17:28:37 -0300

    Criação do ex005

---
+++ exercícios/ex005/ex005.js
@@ -0,0 +1,24 @@
+var n1 = 8
+var n2 = 2
+
+var soma = n1 + n2
+var sub = n1 - n2
+var mult = n1 * n2
+var divisao = n1 / n2
+var resto = n1 % n2
+var expo = n1 ** n2
+
+
+
+
+document.write(`(+) A soma de n1 + n2 é igual a ${soma}.<br>`);
+
+document.write(`(-) A subtração de n1 - n2 é igual a ${sub}.<br>`);
+
+document.write(`(*) A multiplicação de n1 * n2 é igual a ${mult}.<br>`);
+
+document.write(`(/) A divisão n1 / n2 é igual a ${divisao}.<br>`);
+
+document.write(`(%) O resto da divisão de n1 por n2 é igual a ${resto}.<br>`);
+
+document.write(`(**) A exponenciação n1 e n2 é igual a ${expo}.<br>`)
