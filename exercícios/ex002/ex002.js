9f6f0dd8dcb4b923a032d9e4e5da1b27da88e070
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-02-14 12:28:22 -0300

    Exibição do script na tela do HTML

--- exercícios/ex002/ex002.js
+++ exercícios/ex002/ex002.js
@@ -1,9 +1,19 @@
-var nome = 'Gian'
+/*  abrir o terminal com CTRL SHIFT '  */
+/*  digitar "node" (sem aspas) para iniciar  */
+
+/*  CTRL L para limpar o terminal  */
+
+var nome = 'Gian' 
 var idade = '21'
 var cor = 'amarelo'
-var time = 'Palmeiras'
+var time = 'Palmeiras'  /* essas 7 variáveis estão sendo armazenadas */
 var estado = 'São Paulo'
 var cidade = 'Guarulhos'
 var bairro = 'Parque Cecap'
 
+
+/*  _document.write_ significa ESCREVER NO DOCUMENTO: essa string abaixo será apresentada NA TELA(body) do html linkado a esse arquivo .js   */ 
+    document.write(`Oi, meu nome é ${nome}! Tenho ${idade} anos e minha cor preferida é ${cor}.`)
+        
+        /*  ${nome da VAR} é usada como placeholder em TEMPLATE STRING com a string("frase") toda escrita entre CRASES (`...`)  */
 
