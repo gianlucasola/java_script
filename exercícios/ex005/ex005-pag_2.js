0d2866a01e738b1ca159fbf47e5d2bc6f50b9bab
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-02-14 19:16:50 -0300

    Criação das páginas sobre Operadores JavaScript

---
+++ exercícios/ex005/ex005-pag_2.js
@@ -0,0 +1,10 @@
+var n = 3    /*  primeiramente foi atribuído o valor 3 para a VAR chamada nome  */
+n = n + 4    /*  com essa auto-atribuição, o N passa a valer a soma do valor anterior (3) + 4, então agora vale 7   */
+
+
+
+/*  até esta linha, N vale 7 (teve "atualização" em seu valor)  */
+
+
+
+n *= 2    /*  esta simplificação de uma auto-atribuição significa N = N * 2, então o n que até então valia 7 passará a valer 7*2 = 14  */
