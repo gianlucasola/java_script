6890d4a54aa68d3ed74f15149da802332a777b3c
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-02-14 21:42:09 -0300

    adição do tópico sobre incremento javascript

--- exercícios/ex005/ex005-pag_2.js
+++ exercícios/ex005/ex005-pag_2.js
@@ -2,9 +2,20 @@ var n = 3    /*  primeiramente foi atribuído o valor 3 para a VAR chamada nome 
 n = n + 4    /*  com essa auto-atribuição, o N passa a valer a soma do valor anterior (3) + 4, então agora vale 7   */
 
 
-
 /*  até esta linha, N vale 7 (teve "atualização" em seu valor)  */
 
 
-
 n *= 2    /*  esta simplificação de uma auto-atribuição significa N = N * 2, então o n que até então valia 7 passará a valer 7*2 = 14  */
+
+
+
+
+/*  Incremento        
+        v
+   IMPORTANTE: chama-se PÓS-incremento, apenas "n++" NÃO EXIBE a mudança na tela, apenas incrementa "na memória".
+   
+   Para ver a mudança (o acréscimo ou decréscimo) na tela, precisa "perguntar ao JS o valor de n"!!
+                                       */
+
+var n = 9    /*  define a VAR n com o valor 9*/
+n++    /*  acresce 1 ao n (vira 10)  */ 
