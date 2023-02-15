e744f0428b4b1b741bc7119c9907ed757137c52b
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-02-15 15:23:44 -0300

    escrevi linhas no arquivo pag3.js

---
+++ exercícios/ex005/pagina3/pag3.js
@@ -0,0 +1,8 @@
+var n1 = 5
+var n2 = 6
+
+
+n1 > n2 == FALSE    /* (porque n1 não é maior que n2) */
+n1 < n2 == TRUE    /* (porque n1 é menor que n2) */
+
+/* ...assim também com os outros operadores relacionais*/
