424fa74207f877632dc58c27e54e073b68b77a34
Gianluca da Silva Sola <125141081+gianlucasola@users.noreply.github.com>
2023-03-08 16:45:06 -0300

    condições js

---
+++ exercícios/ex008 - Condições/Condição Composta.js
@@ -0,0 +1,7 @@
+var país = 'França'  /*  var de nome país armazena esse texto  */
+
+if (país === 'Brasil'){  /*  se a var país for igual 'Brasil'...  */
+    console.log('Cidadão Brasileiro')  /*  ...console exibirá "Cidadão Brasileiro"  */
+} else{  /*  se não  */
+    console.log('Cidadão Estrangeiro')  /*  ...console exibirá "Cidadão Estrangeiro"  */
+}
---
+++ exercícios/ex008 - Condições/Condição Simples.js
@@ -0,0 +1,8 @@
+var vel = 78.2  /*  var de nome vel  */
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)  /*  exibir esta frase no console  */
+    if(vel > 60){  /*  se a velocidade for maior que 60...  */
+        console.log('Você ultrapassou a velocidade permitida. MULTADO!')  /*  ... esta frase aparecerá escrita no console  */
+    }  /*  CONDIÇÃO SIMPLES: tem _if_ mas não tem _else_  */
+
+    console.log('Dirija sempre usando cinto de segurança')
